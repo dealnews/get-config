e61c36e516b16dcf2a7b625ae56e31bb43cf9874
Brian Moon <brian@moonspot.net>
2023-11-20 12:32:12 -0600

    Add new file types
    * Add support for json, yaml, and env files
    * Add phan, php-cs-fixer, and php-parallel-lint to dev tools

--- src/GetConfig.php
+++ src/GetConfig.php
@@ -2,10 +2,44 @@
 
 namespace DealNews\GetConfig;
 
+use Sarhan\Flatten\Flatten;
+
 /**
- * Gets configuration settings from dealnews.ini
+ * Gets configuration settings
  *
- * Example:
+ * Settings are loaded from environment variables first or ini files second.
+ * The default location for the main ini file is `[app directory]/etc/config.ini`.
+ * Multiple ini files can be loaded from a directory in addition to or instead
+ * of the main ini file. The default directory for loading multiple ini files
+ * is `[app directory]/etc/config.d/`. An environment can be used to override a
+ * single configuration settting, the default ini file location, and the
+ * default ini configuration directory.
+ *
+ * ## Preference Order
+ *
+ * When calling the `get()` method, values are searched for in the following
+ * order:
+ *
+ * 1. Environment Variables - If getting the value of foo.bar, the code will
+ *    look for an environment variable named `foo.bar`, `FOO.BAR`,
+ *    and `FOO_BAR`.
+ * 2. Values in ini files. The last value set will be the value returned. For
+ *    example, if `foo.bar` is set to 100 in `[app directory]/etc/config.ini` and
+ *    `foo.bar` is set to 200 in `[app directory]/etc/config.d/01-other.ini`, the
+ *    `get()` method will return 200 as the files in the config dir are loaded
+ *    after the main ini file. Likewise, if there was a file
+ *    `[app directory]/etc/config.d/02-another.ini` with `foo.bar` set to 300, the
+ *    `get()` method will return 300 as the additional ini files are loaded
+ *    in alphabetical order with the values from last file parsed being used.
+ *
+ * ## Environment Variables
+ *
+ *     DN_INI_FILE - Overrides the default ini file location
+ *     DN_ETC_DIR  - Overrides the directory where config.ini is located
+ *     DN_INI_DIR  - Overrides the default location where mulitple ini
+ *                   files are located
+ *
+ * ## Example Usage
  *
  *     $config = \DealNews\GetConfig\GetConfig::init();
  *     $value1 = $config->get("dealnews.some.var1");
@@ -21,95 +55,280 @@ namespace DealNews\GetConfig;
  * @copyright   1997-Present DealNews.com, Inc
  * @package     GetConfig
  *
+ * @phan-suppress PhanUnreferencedClass
  */
-
 class GetConfig {
 
     /**
-     * ini file name
-     * @var string
+     * Set default dir to what would be a normal base directory
+     * relative to this file being installed via composer.
+     *
+     * @var        string
      */
-    private $ini_file;
+    const DEFAULT_ETC_DIR = __DIR__ . '/../../../../etc';
+
+    /**
+     * Default ini file
+     */
+    const DEFAULT_INI_FILE = self::DEFAULT_ETC_DIR . '/config.ini';
+
+    /**
+     * Default ini directory where multiple ini files can be found
+     */
+    const DEFAULT_INI_DIR = self::DEFAULT_ETC_DIR . '/config.d';
+
+    /**
+     * array of ini file names
+     * @var array
+     */
+    protected array $ini_files = [];
 
     /**
      * ini array
      * @var array
      */
-    private $ini_options = [];
+    protected array $ini_options = [];
 
     /**
-     * Creates the object
-     * @param string $file ini file name
-     */
-    public function __construct(string $file = null) {
-        if ($file === null) {
-            // Check for an environment varible
-            // that has the etc directory
-            $file = getenv("DN_INI_FILE");
-        }
-
-        if (!empty($file)) {
-            if (is_readable($file)) {
-                $this->ini_file = $file;
-            } else {
-                throw new \InvalidArgumentException("File not found or is not readable: $file");
-            }
-        }
-    }
-
-    /**
-     * Gets a value from the environment or ini file
+     * Holds config values found in config files
      *
-     * @param  string $var Config option name
-     * @return mixed       Value or null
+     * @var        array
      */
-    public function get(string $var) {
+    protected array $config = [];
 
-        // always check env vars first as once we read in
-        // an ini file, some values will be set.
-        $check_vars = [
-            $var,
-            strtoupper($var),
-            str_replace(".", "_", strtoupper($var))
-        ];
-        foreach ($check_vars as $check_var) {
-            $val = getenv($check_var);
-            if ($val !== false) {
-                $this->ini_options[$var] = $val;
-                break;
-            }
-        }
+    /**
+     * Location of main config file and config directory
+     *
+     * @var string
+     */
+    protected string $etc_dir = self::DEFAULT_ETC_DIR;
 
-        if (!array_key_exists($var, $this->ini_options)) {
-
-            if (!empty($this->ini_file)) {
-                $this->ini_options = parse_ini_file($this->ini_file);
-                if (!is_array($this->ini_options) || empty($this->ini_options)) {
-                    throw new \RuntimeException("File is not a valid ini file.");
-                }
-                // set to null so we don't parse it twice
-                $this->ini_file = null;
-            }
-
-            // default to null
-            if (!array_key_exists($var, $this->ini_options)) {
-                $this->ini_options[$var] = null;
-            }
-        }
-
-        return $this->ini_options[$var];
-    }
+    /**
+     * Location of additional config files
+     *
+     * @var string
+     */
+    protected string $ini_dir = self::DEFAULT_INI_DIR;
 
     /**
      * Singleton
      *
-     * @return GetConfig
+     * @return self
      */
     public static function init() {
         static $inst;
+
         if (empty($inst)) {
-            $inst  = new GetConfig();
+            $class = get_called_class();
+            $inst  = new $class();
         }
+
         return $inst;
+    }
+
+    /**
+     * Creates the object
+     *
+     * @param ?string $file     config file name
+     * @param ?string $ini_dir  dir were multiple config files can be found
+     */
+    public function __construct(?string $file = null, ?string $ini_dir = null) {
+        if ($file !== null) {
+            if (!file_exists($file)) {
+                throw new \InvalidArgumentException("File not found $file");
+            }
+            $this->etc_dir = dirname($file);
+        } else {
+            // Check for an environment varible
+            // that has the etc directory
+            $file = getenv('DN_INI_FILE');
+            if (empty($file)) {
+                $env_etc_dir = getenv('DN_ETC_DIR');
+                if (!empty($env_etc_dir)) {
+                    $this->etc_dir = $env_etc_dir;
+                    $file          = $this->etc_dir . '/' . basename(self::DEFAULT_INI_FILE);
+                }
+            }
+        }
+
+        if (empty($file)) {
+            if (file_exists(self::DEFAULT_INI_FILE)) {
+                $this->ini_files = [self::DEFAULT_INI_FILE];
+            }
+        } else {
+            if (file_exists($file)) {
+                $this->ini_files = [$file];
+            } else {
+                throw new \InvalidArgumentException("File not found $file");
+            }
+        }
+
+        if ($ini_dir === null) {
+            $ini_dir = getenv('DN_INI_DIR');
+
+            if (empty($ini_dir)) {
+                $ini_dir = $this::DEFAULT_INI_DIR;
+            }
+        }
+
+        if (!empty($ini_dir)) {
+            $this->ini_dir = rtrim($ini_dir, '/');
+            $files         = array_merge(
+                glob("{$this->ini_dir}/*.ini"),
+                glob("{$this->ini_dir}/*.env"),
+                glob("{$this->ini_dir}/*.yaml"),
+                glob("{$this->ini_dir}/*.yml"),
+                glob("{$this->ini_dir}/*.json"),
+            );
+            if (!empty($files)) {
+                sort($files);
+                $this->ini_files = array_merge(
+                    $this->ini_files,
+                    $files
+                );
+            }
+        }
+    }
+
+    /**
+     * Gets a value from the ini file
+     * @param  string $var Config option name
+     * @return ?string     Value or null
+     */
+    public function get(string $var): ?string {
+        if (!array_key_exists($var, $this->config)) {
+            $value = null;
+
+            $check_vars = [
+                str_replace('.', '_', strtoupper($var)),
+                str_replace('.', '_', strtolower($var)),
+                $var,
+                strtoupper($var),
+                strtolower($var),
+            ];
+
+            // Check environment variables first.
+            foreach ($check_vars as $check_var) {
+                $env = getenv($check_var);
+                if ($env !== false) {
+                    $value = $env;
+                    break;
+                }
+            }
+
+            if ($value === null) {
+                if (!empty($this->ini_files)) {
+                    // if the option does not exist, and there
+                    // are files to parse, parse the files and
+                    // check again.
+                    do {
+                        $ini_file = array_shift($this->ini_files);
+
+                        $this->ini_options = array_merge(
+                            $this->ini_options,
+                            $this->parseFile($ini_file)
+                        );
+                    } while (!empty($this->ini_files));
+                }
+
+                // Check for alternate forms of the desired configuration name
+                foreach ($check_vars as $check_var) {
+                    if (array_key_exists($check_var, $this->ini_options)) {
+                        $value = $this->ini_options[$check_var];
+                        break;
+                    }
+                }
+            }
+
+            foreach ($check_vars as $check_var) {
+                $this->config[$check_var] = $value;
+            }
+        }
+
+        return $this->config[$var];
+    }
+
+    /**
+     * Finds a file in the configuration directories
+     *
+     * @param      string       $filename  The filename
+     *
+     * @return     null|string  Returns the found filename or null
+     */
+    public function findFile(string $filename): ?string {
+        $found_file = null;
+
+        if (file_exists($this->etc_dir . '/' . $filename)) {
+            $found_file = $this->etc_dir . '/' . $filename;
+        } elseif (file_exists($this->ini_dir . '/' . $filename)) {
+            $found_file = $this->ini_dir . '/' . $filename;
+        }
+
+        return $found_file;
+    }
+
+    /**
+     * Parses a config file
+     *
+     * @param      string             $file   The file
+     *
+     * @throws     \RuntimeException
+     *
+     * @return     array
+     */
+    protected function parseFile(string $file): array {
+
+        static $flatten;
+
+        $flatten = new Flatten();
+
+        $values = null;
+
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+
+        // Some shared filesystems can cause a file to exist
+        // and not be read or complete at the same time somehow.
+        // So, we try a few times, clearing the stat cache after
+        // each failure.
+        $tries = 0;
+        do {
+            switch ($ext) {
+                case 'yaml':
+                case 'yml':
+                    $values = yaml_parse_file($file);
+                    if (!is_array($values) || empty($values)) {
+                        $values = null;
+                    } else {
+                        $values = $flatten->flattenToArray($values);
+                    }
+                    break;
+                case 'json':
+                    $values = json_decode(file_get_contents($file), true);
+                    if (!is_array($values) || empty($values)) {
+                        $values = null;
+                    } else {
+                        $values = $flatten->flattenToArray($values);
+                    }
+                    break;
+                case 'ini':
+                case 'env': // env files should parse as ini files
+                    $values = @parse_ini_file($file);
+                    break;
+                default:
+                    throw new \RuntimeException('File ' . basename($file) . ' is not a valid configuration file type.');
+            }
+
+            if (!is_array($values)) {
+                clearstatcache();
+            }
+
+            $tries++;
+        } while (!is_array($values) && $tries < 3);
+
+        if (!is_array($values)) {
+            throw new \RuntimeException('File ' . basename($file) . " is not a valid $ext configuration file. Attempted to load it $tries times.");
+        }
+
+        return $values;
     }
 }
--- tests/GetConfigTest.php
+++ tests/GetConfigTest.php
@@ -5,66 +5,271 @@ namespace DealNews\GetConfig\Tests;
 use \DealNews\GetConfig\GetConfig;
 
 class GetConfigTest extends \PHPUnit\Framework\TestCase {
+    public function testFindFile() {
+        $config = new GetConfig(__DIR__ . '/etc/config.ini', __DIR__ . '/etc/config.d/');
 
-    public function testGet() {
+        $this->assertEquals(
+            __DIR__ . '/etc/config.json',
+            $config->findFile('config.json')
+        );
 
-        $config = new GetConfig(__DIR__."/etc/get_config_test.ini");
+        $this->assertEquals(
+            __DIR__ . '/etc/config.d/01-config.ini',
+            $config->findFile('01-config.ini')
+        );
+
+        $this->assertNull($config->findFile('not-found.ini'));
+    }
+
+    public function testGetEnv() {
+        $config = new GetConfig(__DIR__ . '/etc/config.env', '');
 
         // check for an unset value first, should
         // return null and not throw errors
-        $ini = $config->get("not.found");
+        $ini = $config->get('not.found');
 
         $this->assertNull($ini);
 
-        $ini = $config->get("test.string");
+        $ini = $config->get('test.string');
+
+        $this->assertEquals('example-env', $ini);
+    }
+
+    public function testGetYaml() {
+        $config = new GetConfig(__DIR__ . '/etc/config.yaml', '');
+
+        // check for an unset value first, should
+        // return null and not throw errors
+        $ini = $config->get('not.found');
+
+        $this->assertNull($ini);
+
+        $ini = $config->get('test.string');
+
+        $this->assertEquals('example-yaml', $ini);
+    }
+
+    public function testGetJson() {
+        $config = new GetConfig(__DIR__ . '/etc/config.json', '');
+
+        // check for an unset value first, should
+        // return null and not throw errors
+        $ini = $config->get('not.found');
+
+        $this->assertNull($ini);
+
+        $ini = $config->get('test.string');
+
+        $this->assertEquals('example-json', $ini);
+    }
+
+    public function testGetDir() {
+        $DN_INI_FILE = getenv('DN_INI_FILE');
+        $DN_ETC_DIR  = getenv('DN_ETC_DIR');
+
+        putenv('DN_ETC_DIR=');
+        putenv('DN_INI_FILE=');
+
+        putenv('DN_INI_DIR=' . __DIR__ . '/etc/config.d/');
+
+        $config = new GetConfig();
+
+        // check for an unset value first, should
+        // return null and not throw errors
+        $ini = $config->get('not.found');
+
+        $this->assertNull($ini);
+
+        $this->assertEquals('example-config-ini-1', $config->get('test.string'));
+
+        $this->assertEquals('example-config-env-2', $config->get('test.string2'));
+
+        $this->assertEquals('example-config-yaml-3', $config->get('test.string3'));
+
+        $this->assertEquals('example-config-json-4', $config->get('test.string4'));
+
+        $this->assertEquals('yep', $config->get('test.override.me'));
+
+
+        putenv('DN_INI_DIR=');
+        putenv("DN_ETC_DIR=$DN_ETC_DIR");
+        putenv("DN_INI_FILE=$DN_INI_FILE");
+    }
+
+    public function testGetDirPassedIn() {
+        $DN_INI_FILE = getenv('DN_INI_FILE');
+        $DN_ETC_DIR  = getenv('DN_ETC_DIR');
+
+        putenv('DN_ETC_DIR=');
+        putenv('DN_INI_FILE=');
+
+        $config = new GetConfig(null, __DIR__ . '/etc/config.d/');
+
+        // check for an unset value first, should
+        // return null and not throw errors
+        $ini = $config->get('not.found');
+
+        $this->assertNull($ini);
+
+        $this->assertEquals('example-config-ini-1', $config->get('test.string'));
+
+        $this->assertEquals('example-config-env-2', $config->get('test.string2'));
+
+        $this->assertEquals('example-config-yaml-3', $config->get('test.string3'));
+
+        $this->assertEquals('example-config-json-4', $config->get('test.string4'));
+
+        $this->assertEquals('yep', $config->get('test.override.me'));
+
+        putenv("DN_ETC_DIR=$DN_ETC_DIR");
+        putenv("DN_INI_FILE=$DN_INI_FILE");
+    }
+
+    public function testGet() {
+        $DN_INI_FILE = getenv('DN_INI_FILE');
+        $DN_ETC_DIR  = getenv('DN_ETC_DIR');
+
+        putenv('DN_ETC_DIR=');
+        putenv('DN_INI_FILE=');
+
+        $config = new GetConfig(__DIR__ . '/etc/get_config_test.ini');
+
+        // check for an unset value first, should
+        // return null and not throw errors
+        $ini = $config->get('not.found');
+
+        $this->assertNull($ini);
+
+        $ini = $config->get('test.string');
 
         $this->assertEquals('example', $ini);
+
+        putenv("DN_ETC_DIR=$DN_ETC_DIR");
+        putenv("DN_INI_FILE=$DN_INI_FILE");
     }
 
     public function testEnvVarWins() {
+        $config = GetConfig::init();
 
-        $config = new GetConfig();
-
-        $ini = $config->get("dealnews.test.env.var");
+        // defined in both the ini file and in
+        // the phpunit.xml.dist file
+        $ini = $config->get('config.test.env.var');
 
         $this->assertEquals('foo', $ini);
+
+
+        putenv('CONFIG_TEST_ENV_VAR=bar');
+        $config = new GetConfig();
+
+        // check dummy value to load up ini files
+        $config->get('config.test.env.dummy');
+
+        // env var should still be bar even though it has
+        // not been checked yet.
+        $ini = $config->get('config.test.env.var');
+
+        $this->assertEquals('bar', $ini);
+
+        // restore the previous value
+        putenv('CONFIG_TEST_ENV_VAR=foo');
+    }
+
+    public function testEnvDotVar() {
+        $config = new GetConfig();
+
+        $ini = $config->get('config.test.env.dot.var');
+
+        $this->assertEquals('foobar', $ini);
     }
 
     public function testEnvFile() {
-
         $config = new GetConfig();
 
-        $ini = $config->get("test.string");
+        $ini = $config->get('test.string');
 
-        $this->assertEquals('example2', $ini);
+        $this->assertEquals('example-env', $ini);
     }
 
-    public function testEnvVarNoFile() {
-
-        $DN_INI_FILE = getenv("DN_INI_FILE");
-
-        putenv("DN_INI_FILE=");
+    public function testEmptyValue() {
         $config = new GetConfig();
 
-        $ini = $config->get("dealnews.test.env.var");
+        $ini = $config->get('test.empty');
+        $this->assertEquals('', $ini);
 
-        $this->assertEquals('foo', $ini);
+        $ini = $config->get('test.zero');
+        $this->assertEquals(0, $ini);
+    }
+
+    public function testEnvDir() {
+        $DN_INI_FILE = getenv('DN_INI_FILE');
+        putenv('DN_INI_FILE=');
+
+        $config = new GetConfig();
+
+        $ini = $config->get('test.string');
+
+        $this->assertEquals('example-ini', $ini);
 
         putenv("DN_INI_FILE=$DN_INI_FILE");
     }
 
-    /**
-     * @expectedException \InvalidArgumentException
-     */
-    public function testNoFile() {
-        $config = new GetConfig(__DIR__."/bad_filename.ini");
+    public function testEnvVarNoFile() {
+        $DN_INI_FILE = getenv('DN_INI_FILE');
+        $DN_ETC_DIR  = getenv('DN_ETC_DIR');
+
+        putenv('DN_ETC_DIR=');
+        putenv('DN_INI_FILE=');
+        $config = new GetConfig();
+
+        $ini = $config->get('config.test.env.var');
+
+        $this->assertEquals('foo', $ini);
+
+        putenv("DN_ETC_DIR=$DN_ETC_DIR");
+        putenv("DN_INI_FILE=$DN_INI_FILE");
     }
 
-    /**
-     * @expectedException \RuntimeException
-     */
-    public function testBadFile() {
-        $config = new GetConfig(__DIR__."/etc/get_config_empty.ini");
-        $ini = $config->get("test.string");
+    public function testNoFile() {
+        $this->expectException(\InvalidArgumentException::class);
+        $config = new GetConfig(__DIR__ . '/bad_filename.ini');
     }
+
+    public function testBadIniFile() {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessageMatches('/Attempted to load it 3 times\\./');
+        $config = new GetConfig(__DIR__ . '/etc/get_config_bad.ini');
+        $ini    = $config->get('test.string');
+    }
+
+    public function testBadJsonFile() {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessageMatches('/Attempted to load it 3 times\\./');
+        $config = new GetConfig(__DIR__ . '/etc/get_config_bad.json');
+        $ini    = $config->get('test.string');
+    }
+
+    public function testBadYamlFile() {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessageMatches('/Attempted to load it 3 times\\./');
+        $config = new GetConfig(__DIR__ . '/etc/get_config_bad.yaml');
+        $ini    = $config->get('test.string');
+    }
+
+    public function testMissingFileEnvVar() {
+        $DN_INI_FILE = getenv('DN_INI_FILE');
+
+        putenv('DN_INI_FILE=some_missing_filename.ini');
+
+        $this->expectException(\InvalidArgumentException::class);
+        $config = new GetConfig();
+
+        putenv("DN_INI_FILE=$DN_INI_FILE");
+    }
+
+    public function testBadFileType() {
+        $this->expectException(\RuntimeException::class);
+        $config = new GetConfig(__DIR__ . '/etc/get_config_bad.txt');
+        $config->get('foo');
+    }
+
 }
